f308e3f2802eeff79503c5945809c86244fcc552
Filip Ivkovic <fivkovic@uns.ac.rs>
2019-10-31 18:19:23 +0100

    Initial backend project structure created

---
+++ backend/src/main/java/com/siit/ticketist/TicketistApplication.java
@@ -0,0 +1,13 @@
+package com.siit.ticketist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketistApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/siit/ticketist/TicketistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siit.ticketist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
