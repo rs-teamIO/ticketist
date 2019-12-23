b3a8412f42af9ecda6e5dc5284e7d7ed169d6f72
Katarina Tukelic <tukelic.katarina@gmail.com>
2019-12-23 16:46:53 +0100

    Izbaceni nepotrebni importi

--- backend/src/main/java/com/siit/ticketist/dto/InitialReportDTO.java
+++ backend/src/main/java/com/siit/ticketist/dto/InitialReportDTO.java
@@ -6,8 +6,6 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
