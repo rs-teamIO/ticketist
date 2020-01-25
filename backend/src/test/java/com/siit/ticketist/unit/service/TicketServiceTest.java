906110c5dcc94e78eaa8ffb590b1d46a30cf3010
dejandokic <doxa997@gmail.com>
2020-01-25 13:49:27 +0100

    unnecessary imports removed

--- backend/src/test/java/com/siit/ticketist/unit/service/TicketServiceTest.java
+++ backend/src/test/java/com/siit/ticketist/unit/service/TicketServiceTest.java
@@ -14,14 +14,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 
