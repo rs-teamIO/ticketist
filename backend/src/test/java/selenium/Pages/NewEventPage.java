32b67ac598b4c7d4403cfb7e589584d03e5b666d
dejandokic <doxa997@gmail.com>
2020-02-08 02:01:52 +0100

    Create NewEventPage.java

---
+++ backend/src/test/java/selenium/Pages/NewEventPage.java
@@ -0,0 +1,30 @@
+package selenium.Pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NewEventPage {
+    private WebDriver driver;
+
+    @FindBy(xpath = "/html/body/app-root/app-event-form/div/div[1]/form/mat-form-field[1]/div/div[1]/div/input")
+    private WebElement eventName;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+
+    @FindBy(xpath = "")
+    private WebElement ;
+}
